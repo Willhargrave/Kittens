785a23702966c00f0486e392d347377e123ab9c3
Will Hargrave willhargrave@hotmail.co.uk <willhargrave@hotmail.co.uk>
2023-04-06 16:57:07 +0900

    update

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -11,10 +11,19 @@ class KittensController < ApplicationController
   def edit
     @kitten = Kitten.find(params[:id])
   end
+  def update
+    @kitten = Kitten.find(params[:id])
+    if @kitten.update(kitten_params)
+      flash[:notice] = "Kitten editted!"
+      redirect_to @kitten
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
   def destroy
     @kitten = Kitten.find(params[:id])
     @kitten.destroy
-    
+
     redirect_to root_path, status: :see_other, notice: "Successfully deleted kitten!"
   end
 
