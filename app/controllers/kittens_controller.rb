10fe4a87f78fdbfdeeea59b7ab091762fcbab81a
Will Hargrave willhargrave@hotmail.co.uk <willhargrave@hotmail.co.uk>
2023-04-06 17:05:06 +0900

    create

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -11,6 +11,15 @@ class KittensController < ApplicationController
   def edit
     @kitten = Kitten.find(params[:id])
   end
+  def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      redirect_to @kitten
+    else
+      render :new, status: :unprocessable_entity, notice: "Look at that, you failed at filling at a form"
+    end
+  end
   def update
     @kitten = Kitten.find(params[:id])
     if @kitten.update(kitten_params)
